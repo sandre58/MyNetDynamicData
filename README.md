74985c2fa6626915205fe2f92b50c0238d9981d0
Stéphane ANDRE <stephane.andre@soprasteria.com>
2024-04-25 15:04:59 +0200

    chore: Initialization of repository

---
+++ README.md
@@ -0,0 +1,73 @@
+<div id="top"></div>
+
+<!-- PROJECT INFO -->
+<br />
+<div align="center">
+  <a href="https://github.com/sandre58/MyNetDynamicData">
+    <img src="images/logo.png" width="256" height="256">
+  </a>
+
+<h1 align="center">My .NET DynamicData</h1>
+
+[![Downloads][downloads-shield]][downloads-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url]
+
+  <p align="center">
+    <br />
+    The .NET DynamicData is a versatile class library designed to simplify the process of converting objects into human-readable strings in .NET applications. This library provides developers with an easy-to-use interface for generating informative and understandable string representations of complex objects.
+    <br />
+    Supporting only .NET 8.0
+  </p>
+
+[![Language][language-shield]][language-url]
+[![Framework][framework-shield]][framework-url]
+[![Version][version-shield]][version-url]
+[![Build][build-shield]][build-url]
+
+</div>
+
+## Getting Started
+
+To start using My .NET DynamicData in your project, follow these steps:
+
+1. Install the library via NuGet Package Manager:
+   ```bash
+   dotnet add package MyNet.DynamicData.Extensions
+
+## What's included ?
+
+### Dynamic Data Management
+
+- **Dynamic Data Queries**: Build dynamic queries and expressions to filter, sort, and manipulate dynamic data collections with ease.
+
+- **Change Tracking**: Track changes and updates to dynamic data collections in real time, enabling efficient data synchronization and conflict resolution.
+
+## License
+
+Copyright © Stéphane ANDRE.
+
+My .NET DynamicData is provided as-is under the MIT license. For more information see [LICENSE](./LICENSE).
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[language-shield]: https://img.shields.io/github/languages/top/sandre58/MyNetDynamicData
+[language-url]: https://github.com/sandre58/MyNetDynamicData
+[forks-shield]: https://img.shields.io/github/forks/sandre58/MyNetDynamicData?style=for-the-badge
+[forks-url]: https://github.com/sandre58/MyNetDynamicData/network/members
+[stars-shield]: https://img.shields.io/github/stars/sandre58/MyNetDynamicData?style=for-the-badge
+[stars-url]: https://github.com/sandre58/MyNetDynamicData/stargazers
+[issues-shield]: https://img.shields.io/github/issues/sandre58/MyNetDynamicData?style=for-the-badge
+[issues-url]: https://github.com/sandre58/MyNetDynamicData/issues
+[license-shield]: https://img.shields.io/github/license/sandre58/MyNetDynamicData?style=for-the-badge
+[license-url]: https://github.com/sandre58/MyNetDynamicData/blob/main/LICENSE
+[build-shield]: https://img.shields.io/github/actions/workflow/status/sandre58/MyNetDynamicData/ci.yml?logo=github&label=CI
+[build-url]: https://github.com/sandre58/MyNetDynamicData/actions
+[downloads-shield]: https://img.shields.io/github/downloads/sandre58/MyNetDynamicData/total?style=for-the-badge
+[downloads-url]: https://github.com/sandre58/MyNetDynamicData/releases
+[framework-shield]: https://img.shields.io/badge/.NET-8.0-purple
+[framework-url]: https://github.com/sandre58/MyNetDynamicData/tree/main/src/MyNet.DynamicData.Extensions
+[version-shield]: https://img.shields.io/badge/v1.0.0-blue
+[version-url]: https://github.com/sandre58/MyNetDynamicData/pkgs/nuget/MyNet.DynamicData.Extensions
---
+++ src/MyNet.DynamicData.Extensions/MergeManyEx.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (c) Stéphane ANDRE. All Right Reserved.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Linq;
+using System.Reactive.Linq;
+using DynamicData;
+
+namespace My.Utilities.Extensions.DynamicData
+{
+    internal sealed class MergeManyEx<T, TDestination>(IObservable<IChangeSet<T>> source,
+               Func<T, IObservable<IChangeSet<TDestination>>> observableSelector) where T : notnull where TDestination : notnull
+    {
+        private readonly IObservable<IChangeSet<T>> _source = source ?? throw new ArgumentNullException(nameof(source));
+        private readonly Func<T, IObservable<IChangeSet<TDestination>>> _observableSelector = observableSelector ?? throw new ArgumentNullException(nameof(observableSelector));
+
+        private void ForwardWhenRemove(IObserver<IChangeSet<TDestination>> observer, T sourceItem)
+        {
+            var oblist = _observableSelector(sourceItem).AsObservableList();
+            var changeset = new ChangeSet<TDestination>(
+            [
+                new Change<TDestination>(ListChangeReason.RemoveRange, oblist.Items)
+            ]);
+            oblist.Dispose();
+            observer.OnNext(changeset);
+        }
+
+        public IObservable<IChangeSet<TDestination>> Run() => Observable.Create<IChangeSet<TDestination>>
+              (
+                observer =>
+                {
+                    var locker = new object();
+                    return _source
+                // SubscribeMany will forwards inital cascaded inner items when it observers new item,
+                // but if one item was removed, any item belonged to it won't be forwarded to observer.
+                .SubscribeMany(t => _observableSelector(t).Synchronize(locker).Subscribe(observer.OnNext))
+                // So I add a observer here to subscribe all items belonged to the removed item, buildup a 
+                // ChangeSet and and forward it to the original observer.
+                .Subscribe(t =>
+                    {
+                        foreach (var x in t)
+                        {
+                            switch (x.Reason)
+                            {
+                                case ListChangeReason.RemoveRange:
+                                    {
+                                        foreach (var item in x.Range) ForwardWhenRemove(observer, item);
+                                        break;
+                                    }
+                                case ListChangeReason.Remove:
+                                    {
+                                        ForwardWhenRemove(observer, x.Item.Current);
+                                        break;
+                                    }
+                            }
+                        }
+                    }, observer.OnError);
+                });
+    }
+
+    internal sealed class MergeManyEx<T, TKey, TDestination, TDestinationKey>(IObservable<IChangeSet<T, TKey>> source,
+               Func<T, IObservable<IChangeSet<TDestination, TDestinationKey>>> observableSelector,
+               Func<TDestination, TDestinationKey> observableKeySelector) where T : notnull where TDestination : notnull where TKey : notnull where TDestinationKey : notnull
+    {
+        private readonly IObservable<IChangeSet<T, TKey>> _source = source ?? throw new ArgumentNullException(nameof(source));
+        private readonly Func<T, IObservable<IChangeSet<TDestination, TDestinationKey>>> _observableSelector = observableSelector ?? throw new ArgumentNullException(nameof(observableSelector));
+        private readonly Func<TDestination, TDestinationKey> _observableKeySelector = observableKeySelector ?? throw new ArgumentNullException(nameof(observableKeySelector));
+
+        private void ForwardWhenRemove(IObserver<IChangeSet<TDestination, TDestinationKey>> observer, T sourceItem)
+        {
+            var oblist = _observableSelector(sourceItem).AsObservableCache();
+            var changeset = new ChangeSet<TDestination, TDestinationKey>();
+            changeset.AddRange(oblist.Items.Select(x => new Change<TDestination, TDestinationKey>(ChangeReason.Remove, _observableKeySelector.Invoke(x), x)));
+            oblist.Dispose();
+            observer.OnNext(changeset);
+        }
+
+        public IObservable<IChangeSet<TDestination, TDestinationKey>> Run() => Observable.Create<IChangeSet<TDestination, TDestinationKey>>
+              (
+                observer =>
+                {
+                    var locker = new object();
+                    return _source
+                // SubscribeMany will forwards inital cascaded inner items when it observers new item,
+                // but if one item was removed, any item belonged to it won't be forwarded to observer.
+                .SubscribeMany(t => _observableSelector(t).Synchronize(locker).Subscribe(observer.OnNext))
+                // So I add a observer here to subscribe all items belonged to the removed item, buildup a 
+                // ChangeSet and and forward it to the original observer.
+                .Subscribe(t =>
+                {
+                    foreach (var x in t)
+                    {
+                        switch (x.Reason)
+                        {
+                            case ChangeReason.Remove:
+                                {
+                                    ForwardWhenRemove(observer, x.Current);
+                                    break;
+                                }
+                        }
+                    }
+                }, observer.OnError);
+                });
+    }
+
+}
