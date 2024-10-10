c149070823342130c02de4ea3ae6195c649031d7
Stéphane ANDRE (E104915) <stephane.andre_ext@michelin.com>
2024-10-10 19:36:42 +0200

    feat: Add ExtendedObservableCollection and update MyNet.Utilities
    Updated MyNet.Utilities package to version 5.2.1-pre.6
    
    Added a new class ExtendedObservableCollection<T> in
    ExtendedObservableCollection.cs, inheriting from
    OptimizedObservableCollection<T> and implementing
    IObservableCollection<T> and IExtendedList<T> interfaces.

---
+++ src/MyNet.DynamicData.Extensions/ExtendedObservableCollection.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Stéphane ANDRE. All Right Reserved.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using DynamicData;
+using DynamicData.Binding;
+using MyNet.Utilities.Collections;
+
+namespace MyNet.DynamicData.Extensions
+{
+    public class ExtendedObservableCollection<T> : OptimizedObservableCollection<T>, IObservableCollection<T>, IExtendedList<T>
+    {
+        public ExtendedObservableCollection() : base() { }
+
+        public ExtendedObservableCollection(List<T> list) : base(list) { }
+
+        public ExtendedObservableCollection(IEnumerable<T> collection) : base(collection) { }
+    }
+}
